2645406fb7c6b5cfd03e11b57706a7e0a024ef40
Antonio Quintero <man.quintero@gmail.com>
2017-02-21 18:08:49 -0600

    Add relationships

--- organizer/models.py
+++ organizer/models.py
@@ -11,8 +11,10 @@ class Startup(models.Model):
     founded_date = models.DateField()
     contact = models.EmailField()
     website = models.URLField()
+    tags = models.ManoToManyField(Tag)
 
 class NewLink(models.Model):
     title = models.CharField(max_length=63)
     pub_date = models.DateField()
     link = models.URLField()
+    startup = models.ForeignKey(Startup)
